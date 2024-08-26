00653d936949bfb3c006fa2b2cda3993df727f45
rutvik2611 <rutvik2611@gmail.com>
2024-08-25 21:39:00 -0400

    j

--- Aug.py
+++ Aug.py
@@ -1,7 +1,6 @@
 def sum_of_evens(lst):
-# Initialize a variable to store the sum of even numbers.
+    # retrun sum of even numbers
 
-# Loop through each number in the list:
-# If the number is even (use the modulus operator to check), add it to the sum.
 
-# Return the sum of even numbers.
+
+lst =[1,2,3,4,5,6,7,8,9,10,11,20]
